81d63e30a398b02f3b0fdd8b85a3d73f0616437a
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-10-08 15:21:48 -0300

    Estudando migrations

--- app-pweb/routes/web.php
+++ app-pweb/routes/web.php
@@ -4,3 +4,7 @@ Route::get('/', function () {
     return view('home.index')
         ->with(['titulo' => 'Página inicial', 'usuario' => 'Johnny Ferreira', 'n' => 10]);
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
